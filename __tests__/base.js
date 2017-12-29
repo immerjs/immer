d0ef4edf1d0fdf418f79138c4f9d0af3f26d07b5
Michel Weststrate <mweststrate@gmail.com>
2017-12-29 14:09:53 +0100

    Make debugging work

--- __tests__/base.js
+++ __tests__/base.js
@@ -1,5 +1,31 @@
 import immer from ".."
 
-test("base", () => {
-    expect(true).toBe(true)
+describe("base", () => {
+    let baseState
+
+    beforeEach(() => {
+        baseState = createBaseState()
+    })
+
+    it("should return the original without modifications", () => {
+        const nextState = immer(baseState, () => {})
+        expect(nextState).toBe(baseState)
+    })
+
+    afterEach(() => {
+        expect(baseState).toEqual(createBaseState())
+    })
 })
+
+function createBaseState() {
+    return {
+        anArray: [1, 2, { c: 3 }],
+        aProp: "hi",
+        anObject: {
+            nested: {
+                yummie: true
+            },
+            coffee: false
+        }
+    }
+}
