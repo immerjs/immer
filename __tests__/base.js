32710af79842ba19c46e646f57b30a539e37d403
Michel Weststrate <mweststrate@gmail.com>
2018-01-02 23:44:17 +0100

    Removed debug statement

--- __tests__/base.js
+++ __tests__/base.js
@@ -10,7 +10,6 @@ describe("base", () => {
     })
 
     it("should return the original without modifications", () => {
-        debugger
         const nextState = immer(baseState, () => {})
         expect(nextState).toBe(baseState)
     })
