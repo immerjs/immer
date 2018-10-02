2d2ac08a07e97aef30d6dc1e88a455c18f8cabec
Chris Scribner <scriby@gmail.com>
2018-10-02 00:28:32 -0700

    Add immerutable incremental benchmark.

--- __performance_tests__/incremental.js
+++ __performance_tests__/incremental.js
@@ -3,17 +3,19 @@ import {measure} from "./measure"
 import produce, {setAutoFreeze, setUseProxies} from "../dist/immer.umd.js"
 import cloneDeep from "lodash.clonedeep"
 import * as Immutable from "immutable"
+import {SortedMapAdapter} from "immerutable"
 
 console.log("\n# incremental - lot of small incremental changes\n")
 
-function createTestObject() {
+function createTestObject(order) {
     return {
         a: 1,
-        b: "Some data here"
+        b: "Some data here",
+        order
     }
 }
 
-const MAX = 1000
+const MAX = 2000
 const baseState = {
     ids: [],
     map: Object.create(null)
@@ -26,13 +28,25 @@ immutableJsBaseState = {
     map: Immutable.Map()
 }
 
+// An immerutable sorted map is a combination of a map and a list. It maintains
+// items in sorted order and provides key-based retrieval.
+const sortedMapAdapter = new SortedMapAdapter({
+    getOrderingKey: item => item.order
+})
+
+let immerutableBaseState
+
+immerutableBaseState = {
+    sortedMap: sortedMapAdapter.create()
+}
+
 measure(
     "just mutate",
     () => cloneDeep(baseState),
     draft => {
         for (let i = 0; i < MAX; i++) {
             draft.ids.push(i)
-            draft.map[i] = createTestObject()
+            draft.map[i] = createTestObject(i)
         }
     }
 )
@@ -46,7 +60,7 @@ measure(
                 ids: [...state.ids, i],
                 map: {
                     ...state.map,
-                    [i]: createTestObject()
+                    [i]: createTestObject(i)
                 }
             }
         }
@@ -60,8 +74,36 @@ measure(
         for (let i = 0; i < MAX; i++) {
             state = {
                 ids: state.ids.push(i),
-                map: state.map.set(i, createTestObject())
+                map: state.map.set(i, createTestObject(i))
             }
+        }
+    }
+)
+
+measure(
+    "immerutable (sorted map)",
+    () => immerutableBaseState,
+    state => {
+        for (let i = 0; i < MAX; i++) {
+            state = produce(state, draft => {
+                sortedMapAdapter.set(draft.sortedMap, i, createTestObject(i))
+            })
+        }
+    }
+)
+
+measure(
+    "immerutable (sorted map) (reversed)",
+    () => immerutableBaseState,
+    state => {
+        for (let i = 0; i < MAX; i++) {
+            state = produce(state, draft => {
+                sortedMapAdapter.set(
+                    draft.sortedMap,
+                    i,
+                    createTestObject(MAX - i)
+                )
+            })
         }
     }
 )
@@ -77,7 +119,24 @@ measure(
         for (let i = 0; i < MAX; i++) {
             state = produce(state, draft => {
                 draft.ids.push(i)
-                draft.map[i] = createTestObject()
+                draft.map[i] = createTestObject(i)
+            })
+        }
+    }
+)
+
+measure(
+    "immer (proxy) (reversed)",
+    () => {
+        setUseProxies(true)
+        setAutoFreeze(false)
+        return baseState
+    },
+    state => {
+        for (let i = 0; i < MAX; i++) {
+            state = produce(state, draft => {
+                draft.ids.splice(0, 0, i)
+                draft.map[i] = createTestObject(i)
             })
         }
     }
@@ -94,7 +153,7 @@ measure(
         for (let i = 0; i < MAX; i++) {
             state = produce(state, draft => {
                 draft.ids.push(i)
-                draft.map[i] = createTestObject()
+                draft.map[i] = createTestObject(i)
             })
         }
     }
@@ -111,7 +170,7 @@ measure(
         produce(state, draft => {
             for (let i = 0; i < MAX; i++) {
                 draft.ids.push(i)
-                draft.map[i] = createTestObject()
+                draft.map[i] = createTestObject(i)
             }
         })
     }
@@ -128,7 +187,7 @@ measure(
         produce(state, draft => {
             for (let i = 0; i < MAX; i++) {
                 draft.ids.push(i)
-                draft.map[i] = createTestObject()
+                draft.map[i] = createTestObject(i)
             }
         })
     }
