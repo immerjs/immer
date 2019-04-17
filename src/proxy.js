ad4c14c16fa77d0b9b5eab225c03b02dca961e1e
Alec Larson <alec.stanford.larson@gmail.com>
2019-04-17 16:05:17 -0400

    chore: reorganize src/proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -15,6 +15,11 @@ import {ImmerScope} from "./scope"
 // Do nothing before being finalized.
 export function willFinalize() {}
 
+/**
+ * Returns a new draft of the `base` object.
+ *
+ * The second argument is the parent draft-state (used internally).
+ */
 export function createProxy(base, parent) {
     const scope = parent ? parent.scope : ImmerScope.current
     const state = {
@@ -58,17 +63,77 @@ export function createProxy(base, parent) {
     return proxy
 }
 
+/**
+ * Object drafts
+ */
+
 const objectTraps = {
-    get,
+    get(state, prop) {
+        if (prop === DRAFT_STATE) return state
+        let {drafts} = state
+
+        // Check for existing draft in unmodified state.
+        if (!state.modified && has(drafts, prop)) {
+            return drafts[prop]
+        }
+
+        const value = source(state)[prop]
+        if (state.finalized || !isDraftable(value)) {
+            return value
+        }
+
+        // Check for existing draft in modified state.
+        if (state.modified) {
+            // Assigned values are never drafted. This catches any drafts we created, too.
+            if (value !== peek(state.base, prop)) return value
+            // Store drafts on the copy (when one exists).
+            drafts = state.copy
+        }
+
+        return (drafts[prop] = createProxy(value, state))
+    },
     has(target, prop) {
         return prop in source(target)
     },
     ownKeys(target) {
         return Reflect.ownKeys(source(target))
     },
-    set,
-    deleteProperty,
-    getOwnPropertyDescriptor,
+    set(state, prop, value) {
+        if (!state.modified) {
+            const baseValue = peek(state.base, prop)
+            // Optimize based on value's truthiness. Truthy values are guaranteed to
+            // never be undefined, so we can avoid the `in` operator. Lastly, truthy
+            // values may be drafts, but falsy values are never drafts.
+            const isUnchanged = value
+                ? is(baseValue, value) || value === state.drafts[prop]
+                : is(baseValue, value) && prop in state.base
+            if (isUnchanged) return true
+            markChanged(state)
+        }
+        state.assigned[prop] = true
+        state.copy[prop] = value
+        return true
+    },
+    deleteProperty(state, prop) {
+        // The `undefined` check is a fast path for pre-existing keys.
+        if (peek(state.base, prop) !== undefined || prop in state.base) {
+            state.assigned[prop] = false
+            markChanged(state)
+        }
+        if (state.copy) delete state.copy[prop]
+        return true
+    },
+    // Note: We never coerce `desc.value` into an Immer draft, because we can't make
+    // the same guarantee in ES5 mode.
+    getOwnPropertyDescriptor(state, prop) {
+        const owner = source(state)
+        const desc = Reflect.getOwnPropertyDescriptor(owner, prop)
+        if (desc) {
+            desc.writable = true
+            desc.configurable = !Array.isArray(owner) || prop !== "length"
+        }
+        return desc
+    },
     defineProperty() {
         throw new Error("Object.defineProperty() cannot be used on an Immer draft") // prettier-ignore
     },
@@ -79,6 +144,10 @@ const objectTraps = {
         throw new Error("Object.setPrototypeOf() cannot be used on an Immer draft") // prettier-ignore
     }
 }
+
+/**
+ * Array drafts
+ */
 
 const arrayTraps = {}
 each(objectTraps, (key, fn) => {
@@ -100,105 +169,108 @@ arrayTraps.set = function(state, prop, value) {
     return objectTraps.set.call(this, state[0], prop, value)
 }
 
+/**
+ * Map drafts
+ */
+
 const mapTraps = {
     get(state, prop, receiver) {
-        if (prop === DRAFT_STATE) return state
-        if (prop === "size") return source(state)[prop]
-        if (prop === "has") {
-            const stateCurrent = source(state)
-            return stateCurrent[prop].bind(stateCurrent)
-        }
-        if (prop === "set") {
-            return function(key, value) {
-                markChanged(state)
-                state.assigned[key] = true
-                return state.copy.set(key, value)
-            }
-        }
-        if (prop === "delete") {
-            return function(key) {
-                markChanged(state)
-                state.assigned[key] = false
-                return state.copy.delete(key)
-            }
-        }
-        if (prop === "clear") {
-            return function() {
-                markChanged(state)
-                state.assigned = {}
-                for (const key of source(state).keys()) {
-                    state.assigned[key] = false
-                }
-                return state.copy.clear()
-            }
-        }
-        if (prop === "forEach") {
-            return function(cb) {
-                return source(state).forEach((_, key, map) => {
-                    const value = receiver.get(key)
-                    cb(value, key, map)
-                }, this)
-            }
-        }
-        if (prop === "get") {
-            return function(key) {
-                if (!state.modified && has(state.drafts, key)) {
-                    return state.drafts[key]
-                }
-                if (state.modified && state.copy.has(key)) {
-                    return state.copy.get(key)
-                }
-
-                const value = source(state).get(key)
-
-                if (state.finalized || !isDraftable(value)) {
-                    return value
-                }
-
-                const valueProxied = createProxy(value, state)
-                if (!state.modified) {
-                    state.drafts[key] = valueProxied
-                } else {
-                    state.copy.set(key, valueProxied)
-                }
-                return valueProxied
-            }
-        }
-        if (
-            prop === Symbol.iterator ||
-            prop === "entries" ||
-            prop === "values"
-        ) {
-            let getYieldable = (key, value) => [key, value]
-            if (prop === "values") {
-                getYieldable = (key, value) => value
-            }
-            return function*() {
-                const iterator = source(state)[Symbol.iterator]()
-                let result = iterator.next()
-                while (!result.done) {
-                    const [key] = result.value
-                    const value = receiver.get(key)
-                    yield getYieldable(key, value)
-                    result = iterator.next()
-                }
-            }
-        }
-        if (prop === "keys") {
-            return function*() {
-                const iterator = source(state).keys()
-                let result = iterator.next()
-                while (!result.done) {
-                    const key = result.value
-                    yield key
-                    result = iterator.next()
-                }
-            }
-        }
-
-        return Reflect.get(state, prop, receiver)
+        const getter = mapGetters[prop]
+        return getter
+            ? getter(state, prop, receiver)
+            : Reflect.get(state, prop, receiver)
     }
 }
+const mapGetters = {
+    size: state => source(state).size,
+    has(state, prop) {
+        state = source(state)
+        return state.has.bind(state)
+    },
+    set: state => (key, value) => {
+        markChanged(state)
+        state.assigned[key] = true
+        return state.copy.set(key, value)
+    },
+    delete: state => key => {
+        markChanged(state)
+        state.assigned[key] = false
+        return state.copy.delete(key)
+    },
+    clear: state => () => {
+        markChanged(state)
+        state.assigned = {}
+        for (const key of source(state).keys()) {
+            state.assigned[key] = false
+        }
+        return state.copy.clear()
+    },
+    forEach: (state, _, receiver) => (cb, thisArg) =>
+        source(state).forEach((_, key, map) => {
+            const value = receiver.get(key)
+            cb.call(thisArg, value, key, map)
+        }),
+    get: state => key => {
+        if (!state.modified && has(state.drafts, key)) {
+            return state.drafts[key]
+        }
+
+        if (state.modified && state.copy.has(key)) {
+            return state.copy.get(key)
+        }
+
+        const value = source(state).get(key)
+
+        if (state.finalized || !isDraftable(value)) {
+            return value
+        }
+
+        const draft = createProxy(value, state)
+
+        if (!state.modified) {
+            state.drafts[key] = draft
+        } else {
+            state.copy.set(key, draft)
+        }
+
+        return draft
+    },
+    keys(state) {
+        return function*() {
+            const iterator = source(state).keys()
+            let result = iterator.next()
+            while (!result.done) {
+                const key = result.value
+                yield key
+                result = iterator.next()
+            }
+        }
+    },
+    values: iterateMapValues,
+    entries: iterateMapValues,
+    [Symbol.iterator]: iterateMapValues
+}
+
+/** Map.prototype.values _-or-_ Map.prototype.entries */
+function iterateMapValues(state, prop, receiver) {
+    const getYieldable =
+        prop === "values" ? (key, value) => value : (key, value) => [key, value]
+
+    return function*() {
+        const iterator = source(state)[Symbol.iterator]()
+        let result = iterator.next()
+        while (!result.done) {
+            const [key] = result.value
+            const value = receiver.get(key)
+            yield getYieldable(key, value)
+            result = iterator.next()
+        }
+    }
+}
+
+/**
+ * Helpers
+ */
 
 // returns the object we should be reading the current value from, which is base, until some change has been made
 function source(state) {
@@ -213,70 +285,6 @@ function peek(draft, prop) {
         prop
     )
     return desc && desc.value
-}
-
-function get(state, prop) {
-    if (prop === DRAFT_STATE) return state
-    let {drafts} = state
-
-    // Check for existing draft in unmodified state.
-    if (!state.modified && has(drafts, prop)) {
-        return drafts[prop]
-    }
-
-    const value = source(state)[prop]
-    if (state.finalized || !isDraftable(value)) {
-        return value
-    }
-
-    // Check for existing draft in modified state.
-    if (state.modified) {
-        // Assigned values are never drafted. This catches any drafts we created, too.
-        if (value !== peek(state.base, prop)) return value
-        // Store drafts on the copy (when one exists).
-        drafts = state.copy
-    }
-
-    return (drafts[prop] = createProxy(value, state))
-}
-
-function set(state, prop, value) {
-    if (!state.modified) {
-        const baseValue = peek(state.base, prop)
-        // Optimize based on value's truthiness. Truthy values are guaranteed to
-        // never be undefined, so we can avoid the `in` operator. Lastly, truthy
-        // values may be drafts, but falsy values are never drafts.
-        const isUnchanged = value
-            ? is(baseValue, value) || value === state.drafts[prop]
-            : is(baseValue, value) && prop in state.base
-        if (isUnchanged) return true
-        markChanged(state)
-    }
-    state.assigned[prop] = true
-    state.copy[prop] = value
-    return true
-}
-
-function deleteProperty(state, prop) {
-    // The `undefined` check is a fast path for pre-existing keys.
-    if (peek(state.base, prop) !== undefined || prop in state.base) {
-        state.assigned[prop] = false
-        markChanged(state)
-    }
-    if (state.copy) delete state.copy[prop]
-    return true
-}
-
-// Note: We never coerce `desc.value` into an Immer draft, because we can't make
-// the same guarantee in ES5 mode.
-function getOwnPropertyDescriptor(state, prop) {
-    const owner = source(state)
-    const desc = Reflect.getOwnPropertyDescriptor(owner, prop)
-    if (desc) {
-        desc.writable = true
-        desc.configurable = !Array.isArray(owner) || prop !== "length"
-    }
-    return desc
 }
 
 function markChanged(state) {
