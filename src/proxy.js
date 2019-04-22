a355b3e201e792134100cc294fc87b778a5824c9
keenondrums <andrey.goncharov+it@protonmail.com>
2019-04-22 09:49:31 +0300

    nit: Refactor creating traps for Maps and Sets

--- src/proxy.js
+++ src/proxy.js
@@ -187,13 +187,6 @@ arrayTraps.set = function(state, prop, value) {
  * Map drafts
  */
 
-const mapTraps = {
-    get(state, prop, receiver) {
-        return mapGetters.hasOwnProperty(prop)
-            ? mapGetters[prop](state, prop, receiver)
-            : Reflect.get(state, prop, receiver)
-    }
-}
 const mapGetters = {
     [DRAFT_STATE]: state => state,
     size: state => source(state).size,
@@ -258,6 +251,7 @@ const mapGetters = {
     entries: iterateMapValues,
     [Symbol.iterator]: iterateMapValues
 }
+const mapTraps = makeTrapsForGetters(mapGetters)
 
 /** Map.prototype.values _-or-_ Map.prototype.entries */
 function iterateMapValues(state, prop, receiver) {
@@ -282,16 +276,6 @@ function iterateMapValues(state, prop, receiver) {
  * Set drafts
  */
 
-const setTraps = {
-    get(state, prop, receiver) {
-        return setGetters.hasOwnProperty(prop)
-            ? setGetters[prop](state, prop, receiver)
-            : Reflect.get(state, prop, receiver)
-    },
-    ownKeys(state) {
-        return Reflect.ownKeys(source(state))
-    }
-}
 const setGetters = {
     [DRAFT_STATE]: state => state,
     size: state => source(state).size,
@@ -327,6 +311,7 @@ const setGetters = {
     entries: iterateSetValues,
     [Symbol.iterator]: iterateSetValues
 }
+const setTraps = makeTrapsForGetters(setGetters)
 
 function iterateSetValues(state, prop) {
     return () => {
@@ -416,4 +401,17 @@ function makeIterable(next) {
         [Symbol.iterator]: () => self,
         next
     })
+}
+
+function makeTrapsForGetters(getters) {
+    return {
+        get(state, prop, receiver) {
+            return getters.hasOwnProperty(prop)
+                ? getters[prop](state, prop, receiver)
+                : Reflect.get(state, prop, receiver)
+        },
+        ownKeys(state) {
+            return Reflect.ownKeys(source(state))
+        }
+    }
 }
