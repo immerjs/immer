79ec1971d32eb2dc771d3bb84f6d427e37be1d94
Michel Weststrate <mweststrate@gmail.com>
2019-09-12 13:15:53 +0200

    WIP

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,44 +8,24 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-	{
-		caption: "User1",
-		// You will need to prepend the image path with your baseUrl
-		// if it is not '/', like: '/test-site/img/image.jpg'.
-		image: "/img/undraw_open_source.svg",
-		infoLink: "https://www.facebook.com",
-		pinned: true
-	}
-]
-
 const siteConfig = {
-	title: "Test Site", // Title for your website.
-	tagline: "A website for testing",
-	url: "https://your-docusaurus-test-site.com", // Your website URL
-	baseUrl: "/", // Base URL for your project */
+	title: "Immer", // Title for your website.
+	tagline: "Create the next immutable state by mutating the current one",
+	url: "https://immerjs.github.io", // Your website URL
+	baseUrl: "/immer/", // Base URL for your project */
 	// For github.io type URLs, you would set the url and baseUrl like:
 	//   url: 'https://facebook.github.io',
 	//   baseUrl: '/test-site/',
 
 	// Used for publishing and more
-	projectName: "test-site",
-	organizationName: "facebook",
+	projectName: "immer",
+	organizationName: "immerjs",
 	// For top-level user or org sites, the organization is still the same.
 	// e.g., for the https://JoelMarcey.github.io site, it would be set like...
 	//   organizationName: 'JoelMarcey'
 
 	// For no header links in the top nav bar -> headerLinks: [],
-	headerLinks: [
-		{doc: "doc1", label: "Docs"},
-		{doc: "doc4", label: "API"},
-		{page: "help", label: "Help"},
-		{blog: true, label: "Blog"}
-	],
-
-	// If you have users set above, you add it here:
-	users,
+	headerLinks: [{doc: "doc1", label: "Docs"}, {page: "help", label: "Help"}],
 
 	/* path to images for header/footer */
 	headerIcon: "img/favicon.ico",
@@ -54,8 +34,8 @@ const siteConfig = {
 
 	/* Colors for website */
 	colors: {
-		primaryColor: "#11220f",
-		secondaryColor: "#0b170a"
+		primaryColor: "#000",
+		secondaryColor: "#22e7c3"
 	},
 
 	/* Custom fonts for website */
@@ -73,11 +53,11 @@ const siteConfig = {
   */
 
 	// This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-	copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+	copyright: `Copyright © ${new Date().getFullYear()} Michel Weststrate`,
 
 	highlight: {
 		// Highlight.js theme to use for syntax highlighting in code blocks.
-		theme: "default"
+		theme: "dracula"
 	},
 
 	// Add custom scripts here that would be placed in <script> tags.
