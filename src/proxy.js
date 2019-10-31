2409fe559099a54a24aa2f77be3762a29defff27
Dalci de Jesus Bagolin <dalcib@users.noreply.github.com>
2019-10-31 16:52:12 +0530

    Update 5.0.0

--- src/proxy.js
+++ src/proxy.js
@@ -107,6 +107,10 @@ const objectTraps = {
 			// Store drafts on the copy (when one exists).
 			drafts = state.copy
 		}
+		
+		if (!Object.keys(state.base).includes(prop)) {
+			return value
+		}
 
 		return (drafts[prop] = createProxy(value, state))
 	},
