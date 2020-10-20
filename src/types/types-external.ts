12f4cf8a107e89645fe65833a359a53f36e01103
Lenz Weber <mail@lenzw.de>
2020-10-20 21:31:16 +0200

    fix: skip ReadonlyMap and ReadonlySet types when not available (#653). Fixes #624

--- src/types/types-external.ts
+++ src/types/types-external.ts
@@ -18,18 +18,35 @@ type AtomicObject =
 	| String
 
 /**
+ * If the lib "ES2105.collections" is not included in tsconfig.json,
+ * types like ReadonlyArray, WeakMap etc. fall back to `any` (specified nowhere)
+ * or `{}` (from the node types), in both cases entering an infite recursion in
+ * pattern matching type mappings
+ * This type can be used to cast these types to `void` in these cases.
+ */
+export type IfAvailable<T, Fallback = void> =
+	// fallback if any
+	true | false extends (T extends never
+	? true
+	: false)
+		? Fallback // fallback if empty type
+		: keyof T extends never
+		? Fallback // original type
+		: T
+
+/**
  * These should also never be mapped but must be tested after regular Map and
  * Set
  */
-type WeakReferences = WeakMap<any, any> | WeakSet<any>
+type WeakReferences = IfAvailable<WeakMap<any, any>> | IfAvailable<WeakSet<any>>
 
 export type WritableDraft<T> = {-readonly [K in keyof T]: Draft<T[K]>}
 
 export type Draft<T> = T extends AtomicObject
 	? T
-	: T extends ReadonlyMap<infer K, infer V> // Map extends ReadonlyMap
+	: T extends IfAvailable<ReadonlyMap<infer K, infer V>> // Map extends ReadonlyMap
 	? Map<Draft<K>, Draft<V>>
-	: T extends ReadonlySet<infer V> // Set extends ReadonlySet
+	: T extends IfAvailable<ReadonlySet<infer V>> // Set extends ReadonlySet
 	? Set<Draft<V>>
 	: T extends WeakReferences
 	? T
@@ -40,9 +57,9 @@ export type Draft<T> = T extends AtomicObject
 /** Convert a mutable type into a readonly type */
 export type Immutable<T> = T extends AtomicObject
 	? T
-	: T extends ReadonlyMap<infer K, infer V> // Map extends ReadonlyMap
+	: T extends IfAvailable<ReadonlyMap<infer K, infer V>> // Map extends ReadonlyMap
 	? ReadonlyMap<Immutable<K>, Immutable<V>>
-	: T extends ReadonlySet<infer V> // Set extends ReadonlySet
+	: T extends IfAvailable<ReadonlySet<infer V>> // Set extends ReadonlySet
 	? ReadonlySet<Immutable<V>>
 	: T extends WeakReferences
 	? T
