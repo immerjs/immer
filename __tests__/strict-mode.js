c30fef3bfefcf1feddb01befad067583e7849b0a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-12-03 23:09:10 +0100

    Add tests for strict mode

---
+++ __tests__/strict-mode.js
@@ -0,0 +1,106 @@
+"use strict"
+import produce, {
+	setStrictMode,
+	unsafe,
+	immerable,
+	enableMapSet
+} from "../src/immer"
+
+enableMapSet()
+
+describe("Strict Mode", () => {
+	class Foo {}
+
+	describe("by default", () => {
+		it("should not throw an error when accessing a non-draftable class instance", () => {
+			expect(() =>
+				produce({instance: new Foo()}, draft => {
+					draft.instance.value = 5
+				})
+			).not.toThrow()
+		})
+	})
+
+	afterAll(() => {
+		setStrictMode(false)
+	})
+
+	describe("when disabled", () => {
+		beforeEach(() => {
+			setStrictMode(false)
+		})
+
+		it("should allow accessing a non-draftable class instance", () => {
+			expect(() =>
+				produce({instance: new Foo()}, draft => {
+					draft.instance.value = 5
+				})
+			).not.toThrow()
+		})
+
+		it("should not throw errors when using the `unsafe` function", () => {
+			expect(() =>
+				produce({instance: new Foo()}, draft => {
+					unsafe(() => {
+						draft.instance.value = 5
+					})
+				})
+			).not.toThrow()
+		})
+	})
+
+	describe("when enabled", () => {
+		beforeEach(() => {
+			setStrictMode(true)
+		})
+
+		it("should throw an error when accessing a non-draftable class instance", () => {
+			expect(() =>
+				produce({instance: new Foo()}, draft => {
+					draft.instance
+				})
+			).toThrow()
+		})
+
+		it("should allow accessing a non-draftable using the `unsafe` function", () => {
+			expect(() =>
+				produce({instance: new Foo()}, draft => {
+					unsafe(() => {
+						draft.instance.value = 5
+					})
+				})
+			).not.toThrow()
+		})
+
+		describe("with an immerable class", () => {
+			beforeAll(() => {
+				Foo[immerable] = true
+			})
+
+			afterAll(() => {
+				Foo[immerable] = true
+			})
+
+			it("should allow accessing the class instance", () => {
+				expect(() =>
+					produce({instance: new Foo()}, draft => {
+						draft.instance.value = 5
+					})
+				).not.toThrow()
+			})
+		})
+
+		it("should allow accessing draftable properties", () => {
+			expect(() =>
+				produce({arr: [], obj: {}, map: new Map(), set: new Set()}, draft => {
+					draft.arr.push(1)
+					draft.arr[0] = 1
+					draft.obj.foo = 5
+					draft.obj.hasOwnProperty("abc")
+					draft.map.set("foo", 5)
+					draft.set.add("foo")
+				})
+			).not.toThrow()
+		})
+	})
+})
