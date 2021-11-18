abba6805e7c073d477deaa55d44cb1b18fbc3fc9
Louis DeScioli <louis@spatial.io>
2021-11-17 19:49:11 -0500

    Appends to array when key is "-"

--- src/core/immerClass.ts
+++ src/core/immerClass.ts
@@ -200,12 +200,12 @@ export class Immer implements ProducersFns {
 		const applyPatchesImpl = getPlugin("Patches").applyPatches_
 		if (isDraft(base)) {
 			// N.B: never hits if some patch a replacement, patches are never drafts
-			return applyPatchesImpl(base, patches) as any
+			return applyPatchesImpl(base, patches)
 		}
 		// Otherwise, produce a copy of the base state.
 		return this.produce(base, (draft: Drafted) =>
 			applyPatchesImpl(draft, patches.slice(i + 1))
-		) as any
+		)
 	}
 }
 
