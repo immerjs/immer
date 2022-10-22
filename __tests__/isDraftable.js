8aac6ca35dd3c89457d9059019653bdecc362e99
Michel Weststrate <mweststrate@fb.com>
2022-10-22 15:12:09 +0200

    chore: fix build / test issue

--- __tests__/isDraftable.js
+++ __tests__/isDraftable.js
@@ -1,7 +1,7 @@
 "use strict"
-import { isDraftable } from "../src/immer"
+import {isDraftable} from "../src/immer"
 
 test("non-plain object with undefined constructor doesn't error", () => {
-    const obj = Object.create(Object.create(null))
-    expect(isDraftable(obj).toBe(true);
+	const obj = Object.create(Object.create(null))
+	expect(isDraftable(obj)).toBe(false)
 })
