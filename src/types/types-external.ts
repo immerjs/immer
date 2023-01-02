c88f787b038e4c74631fdec2e74c2b4b2d4b6f2f
feihe <feihe08@gmail.com>
2023-01-03 04:07:07 +0800

    feature: Export the type of a recipe as `Produce` (#968)
    * feature: add type for producer function
    
    * IProducer -> Producer
    
    Co-authored-by: Michel Weststrate <mweststrate@gmail.com>

--- src/types/types-external.ts
+++ src/types/types-external.ts
@@ -252,6 +252,11 @@ export interface IProduceWithPatches {
 	): Promise<PatchesTuple<Base>>
 }
 
+/**
+ * The type for `recipe function`
+ */
+export type Producer<T> = (draft: Draft<T>) => ValidRecipeReturnType<Draft<T>> | Promise<ValidRecipeReturnType<Draft<T>>>
+
 // Fixes #507: bili doesn't export the types of this file if there is no actual source in it..
 // hopefully it get's tree-shaken away for everyone :)
 export function never_used() {}
