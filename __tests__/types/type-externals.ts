fd1f29b91b095b7904c381bf18af06fb2858db46
Nathan Bierema <nbierema@gmail.com>
2023-01-16 09:40:33 -0500

    Revert  #990

--- __tests__/types/type-externals.ts
+++
@@ -1,9 +0,0 @@
-import {isType, JSONTypes} from "type-plus"
-import {Draft} from "../../src/types/types-external"
-
-describe("Draft<T>", () => {
-	test("can use JSONTypes as T", () => {
-		type A = Draft<JSONTypes>
-		isType.equal<true, JSONTypes, A>()
-	})
-})
