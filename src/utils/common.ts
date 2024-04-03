9b79e07a6738f1f61dc5263c289344a7b4ce3caf
Fredrik Vaeng Røtnes <frwdrik@gmail.com>
2024-04-03 18:48:54 +0000

    Update src/utils/common.ts
    Co-authored-by: Steve Ognibene <steve.ognibene@namely.com>

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -62,7 +62,7 @@ export function original(value: Drafted<any>): any {
 
 /**
  * Each iterates a map, set or array.
- * Or, if any other kind of object, all of it's own properties.
+ * Or, if any other kind of object, all of its own properties.
  * Regardless whether they are enumerable or symbols
  */
 export function each<T extends Objectish>(
