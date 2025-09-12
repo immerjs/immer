0f237bd1a90793d3482a1fc470ba0be647dc2806
Michel Weststrate <mweststrate@meta.com>
2025-09-12 12:30:54 +0200

    chore: fix #3

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
 		environment: "node",
 		include: ["**/__tests__/**/*.[jt]s?(x)"],
 		globals: true,
-		setupFiles: []
+		setupFiles: [],
+		coverage: {
+			provider: "v8", // default in Vitest 3
+			reporter: ["text", "lcov"], // add "lcov" here
+			reportsDirectory: "./coverage" // makes sure lcov.info lands here
+		}
 	}
 })
